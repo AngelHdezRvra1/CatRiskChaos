7a3280094819a6d66e12a5712afd673218ca75ec
AngelHdezRvra1 <angel2003213@gmail.com>
2026-06-15 16:07:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="CatRisk Chaos",
+    layout="wide"
+)
+
 st.title("CatRisk Chaos")
-st.write("Sistema de Riesgo Catastrófico")
+st.subheader(
+    "Simulación de Riesgos Catastróficos con Teoría del Caos"
+)
+
+st.write(
+    "Aplicación desarrollada para el Trabajo Terminal."
+)
